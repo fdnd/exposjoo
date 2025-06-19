9965af373168fe9cbf13a2d311c709f407ca1fc7
Cyd Stumpel <info@cydstumpel.nl>
2025-06-19 10:46:12 +0200

    add seo things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,6 +22,27 @@
   })
 </script>
 
+<svelte:head>
+  <title>
+    Exposjoo - a typical Amsterdam digital design sjoo
+  </title>
+  <meta name="description" content="Exposjoo is a typical Amsterdam digital design sjoo hosted by the Amsterdam University of Applied Sciences." />
+  <meta name="keywords" content="digital design, development, sjoo, amsterdam" />
+  <!-- social image -->
+  <meta property="og:image" content="https://exposjoo.nl/social-image.png" />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="675" />
+  <meta property="og:image:alt" content="Exposjoo, 2 July 2025, 17-19h, Theo Thijssenhuis Amsterdam" />
+  <meta property="og:image:type" content="image/png" />
+  <meta property="og:image:secure_url" content="https://exposjoo.nl/social-image.png" />
+  <meta property="og:title" content="Exposjoo - a typical Amsterdam digital design sjoo" />
+  <meta property="og:description" content="Exposjoo is a typical Amsterdam digital design sjoo hosted by the Amsterdam University of Applied Sciences." />
+  <meta property="og:url" content="https://exposjoo.nl" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name" content="Exposjoo" />
+  <meta property="og:locale" content="en_US" />
+</svelte:head>
+
 <Intro on:introComplete={handleIntroComplete} />
 <div class="text">
   <IntroText text={data.introText} />
