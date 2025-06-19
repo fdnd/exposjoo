0edca7db050326cfe06c7effbd13db4be1113ba2
Cyd Stumpel <info@cydstumpel.nl>
2025-06-19 10:56:26 +0200

    last try

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,53 @@
 <script>
-  import Intro from '$lib/components/Intro.svelte'
-  import Building from '$lib/components/Building.svelte'
-  import IntroText from '$lib/components/IntroText.svelte'
-  import { onMount } from 'svelte'
-  import { data } from './data'
+  import Intro from "$lib/components/Intro.svelte";
+  import Building from "$lib/components/Building.svelte";
+  import IntroText from "$lib/components/IntroText.svelte";
+  import { onMount } from "svelte";
+  import { data } from "./data";
 
-  let buildingComponent
-  let introComplete = false
+  let buildingComponent;
+  let introComplete = false;
 
   function handleIntroComplete() {
-    introComplete = true
+    introComplete = true;
     if (buildingComponent) {
-      buildingComponent.startBuildingAnimation()
+      buildingComponent.startBuildingAnimation();
     }
   }
 
   onMount(() => {
     if (introComplete && buildingComponent) {
-      buildingComponent.startBuildingAnimation()
+      buildingComponent.startBuildingAnimation();
     }
-  })
+  });
 </script>
 
 <svelte:head>
-  <title>
-    Exposjoo - a typical Amsterdam digital design sjoo
-  </title>
-  <meta name="description" content="Exposjoo is a typical Amsterdam digital design sjoo hosted by the Amsterdam University of Applied Sciences." />
-  <meta name="keywords" content="digital design, development, sjoo, amsterdam" />
-  <!-- social image -->
-  <meta property="og:image" content="https://exposjoo.nl/social-image.jpg" />
-  <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="675" />
-  <meta property="og:image:alt" content="Exposjoo, 2 July 2025, 17-19h, Theo Thijssenhuis Amsterdam" />
-  <meta property="og:image:type" content="image/jpeg" />
-  <meta property="og:image:secure_url" content="https://exposjoo.nl/social-image.jpg" />
+  <title>Exposjoo - a typical Amsterdam digital design sjoo</title>
+  <meta
+    name="description"
+    content="Exposjoo is a typical Amsterdam digital design sjoo hosted by the Amsterdam University of Applied Sciences."
+  />
+  <meta
+    name="keywords"
+    content="digital design, development, sjoo, amsterdam"
+  />
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://exposjoo.nl/" />
+  <meta property="og:type" content="website" />
   <meta property="og:title" content="Exposjoo - a typical Amsterdam digital design sjoo" />
   <meta property="og:description" content="Exposjoo is a typical Amsterdam digital design sjoo hosted by the Amsterdam University of Applied Sciences." />
-  <meta property="og:url" content="https://exposjoo.nl" />
-  <meta property="og:type" content="website" />
-  <meta property="og:site_name" content="Exposjoo" />
-  <meta property="og:locale" content="en_US" />
+  <meta property="og:image" content="https://exposjoo.nl/social-image.jpg" />
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta property="twitter:domain" content="exposjoo.nl" />
+  <meta property="twitter:url" content="https://exposjoo.nl/" />    
+  <meta name="twitter:title" content="Exposjoo - a typical Amsterdam digital design sjoo" />
+  <meta name="twitter:description" content="Exposjoo is a typical Amsterdam digital design sjoo hosted by the Amsterdam University of Applied Sciences." />
+  <meta name="twitter:image" content="https://exposjoo.nl/social-image.jpg" />
+
 </svelte:head>
 
 <Intro on:introComplete={handleIntroComplete} />
@@ -48,7 +55,6 @@
   <IntroText text={data.introText} />
 </div>
 <Building bind:this={buildingComponent} courses={data.courses} />
-
 
 <style>
 </style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,19 +9,20 @@
   </title>
   <meta name="description" content="Exposjoo is an evening in which we proudly present the student work of the Associate Degreed Frontend Design and Development, the Master Digital Design and the Bachelor Communication and Multimedia Design." />
   <meta name="keywords" content="digital design, development, sjoo, amsterdam, university of applied sciences" />
-  <!-- social image -->
-  <meta property="og:image" content="https://exposjoo.nl/social-image.jpg" />
-  <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="675" />
-  <meta property="og:image:alt" content="Exposjoo, 2 July 2025, 17-19h, Theo Thijssenhuis Amsterdam" />
-  <meta property="og:image:type" content="image/jpeg" />
-  <meta property="og:image:secure_url" content="https://exposjoo.nl/social-image.jpg" />
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://exposjoo.nl/" />
+  <meta property="og:type" content="website" />
   <meta property="og:title" content="Exposjoo - a typical Amsterdam digital design sjoo" />
   <meta property="og:description" content="Exposjoo is a typical Amsterdam digital design sjoo hosted by the Amsterdam University of Applied Sciences." />
-  <meta property="og:url" content="https://exposjoo.nl" />
-  <meta property="og:type" content="website" />
-  <meta property="og:site_name" content="Exposjoo" />
-  <meta property="og:locale" content="en_US" />
+  <meta property="og:image" content="https://exposjoo.nl/social-image.jpg" />
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta property="twitter:domain" content="exposjoo.nl" />
+  <meta property="twitter:url" content="https://exposjoo.nl/" />    
+  <meta name="twitter:title" content="Exposjoo - a typical Amsterdam digital design sjoo" />
+  <meta name="twitter:description" content="Exposjoo is a typical Amsterdam digital design sjoo hosted by the Amsterdam University of Applied Sciences." />
+  <meta name="twitter:image" content="https://exposjoo.nl/social-image.jpg" />
 </svelte:head>
 
 <Text textContainers={data.textContainers} />
